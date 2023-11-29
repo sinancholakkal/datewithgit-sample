e9f9beadc477941625b366b2a64a307fa48b769f
Muhammed sinan <sinanzzsinu70@gmail.com>
2023-11-29 06:29:05 +0530

    added some massages

--- main.dart
+++ main.dart
@@ -21,7 +21,7 @@ class Screen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(title: Text('Sample'),),
     );
   }
 }
