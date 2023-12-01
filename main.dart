2863fb50e9195e2dd5dcde56bbdfb6750a8cef0a
Muhammed sinan <sinanzzsinu70@gmail.com>
2023-12-01 06:03:01 +0530

    main file modified

--- main.dart
+++ main.dart
@@ -1,27 +1,3 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MyApp());
-}
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colors.amber
-      ),home: Screen(),
-    );
-  }
-}
-class Screen extends StatelessWidget {
-  const Screen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Sample'),),
-    );
-  }
-}
+can we meet tomarrow?
